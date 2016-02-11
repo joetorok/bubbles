2aec7ed7ecb1805884bb9fc725cfb3662e92e73d
Joe Torok <fm7222@wayne.edu>
2016-02-11 17:08:58 -0500

    using while loop conditional

--- sketch.js
+++ sketch.js
@@ -1,11 +1,29 @@
 var bubbles = [];
 
-setup = function () {
-  createCanvas(600, 600);
+/*var updateAndDisplay = function(bubble) {
+  bubble.update();
+  bubble.display();
+}*/
+
+var setup = function () {
+  createCanvas(700, 700);
   // your code goes here
 };
 
-draw = function () {
+var draw = function () {
   background(0);
   // your code goes here
+  var index = 0;
+  while (index < bubbles.length) {
+    bubbles[index].update();
+    bubbles[index].display();
+    index += 1;
+  }
+  if (mouseIsPressed) bubbles[bubbles.length] = new Bubble(mouseX, mouseY);
+
+
+
+
+  //bubbles.forEach(updateAndDisplay);
+  //if (mouseIsPressed) bubbles.push(new Bubble(mouseX, mouseY));
 };
