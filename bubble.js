8386b38f1cf0a64e0bae72c0b40bc9d1aa99bda7
Joe Torok <fm7222@wayne.edu>
2016-02-11 16:40:18 -0500

    bubbles move up

--- bubble.js
+++ bubble.js
@@ -10,7 +10,7 @@ Bubble.prototype = {
   update: function() {
     this.x += random(-1, 1);
     this.y += random(0, -2);
-    if (this.y - this.radius <= 0) this.y = this.radius + random(0, 2);
+    
   },
 
   display: function () {
