f3135e1c8e1dd2bcf7a714bc19eb665f815f5de4
Joe Torok <fm7222@wayne.edu>
2016-02-11 21:52:03 -0500

    500 bubbles

--- sketch.js
+++ sketch.js
@@ -1,11 +1,40 @@
 var bubbles = [];
 
+var updateAndDisplay = function(bubble) {
+  bubble.update();
+  bubble.display();
+};
+
+var addNumberBubbles = function (totalBubbles) {
+  for (var i = 0; i < totalBubbles; ++i) {
+    bubbles[i] = new Bubble(random(0, width), random(0, height));
+  }
+};
+
 setup = function () {
   createCanvas(600, 600);
   // your code goes here
+  addNumberBubbles(500);
 };
 
 draw = function () {
   background(0);
-  // your code goes here
+
+  bubbles.forEach(updateAndDisplay);
 };
+  // your code goes here
+
+
+    //bubbles[index].update();
+    //bubbles[index].draw();
+
+
+/* -- using while loop
+  while (bubbles.length < totalBubbles) P
+    bubbles.push(new Bubble(random(0, width), random (0,height)));
+
+
+/*
+  bubbles.forEach(updateAndDisplay);
+  if (mouseIsPressed) bubbles.push(new Bubble(mouseX, mouseY));
+*/
