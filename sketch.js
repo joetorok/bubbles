970fb404a10d7dca83699246e2e6aadfe990041f
Joe Torok <fm7222@wayne.edu>
2016-02-18 21:16:01 -0500

    done

--- sketch.js
+++ sketch.js
@@ -5,14 +5,33 @@ var updateAndDisplay = function(bubble) {
   bubble.display();
 }
 
-var setup = function () {
+setup = function () {
   createCanvas(700, 700);
   // your code goes here
 };
 
-var draw = function () {
+draw = function () {
   background(0);
   // your code goes here
   bubbles.forEach(updateAndDisplay);
   if (mouseIsPressed) bubbles.push(new Bubble(mouseX, mouseY));
 };
+
+/* -- without vars
+
+  bubbles.forEach(function updateAndDisplay(bubble) {
+    bubble.uodate();
+    bubble.display();
+
+})
+
+*/
+
+  /* --  for loop xample with this sketch
+  if (mouseIsPressed) bubbles.push(new Bubble (mouseX, mouseY));
+  for (var i = 0; i < bubbles.length; ++i){
+    bubbles[i].update();
+    bubbles[i].display();
+
+}
+*/
