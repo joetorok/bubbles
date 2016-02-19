21a6eb98d0d978f3d18a1a1b9a51f2532585bf29
Joe Torok <fm7222@wayne.edu>
2016-02-18 20:22:42 -0500

    toxic rain

--- bubble.js
+++ bubble.js
@@ -1,22 +1,28 @@
-var Bubble = function (x, y) {
+var Rain = function (x, y) {
   this.x = x;
   this.y = y;
+
 };
 
-Bubble.prototype = {
+Rain.prototype = {
 
-  radius: 5,
+  rainWidth: 1,
+  rainHeight: 20,
+  rainOffset: 8,
 
   update: function() {
-    this.x += random(-1, 1);
-    this.y += random(0, -2);
-    if (this.y - this.radius <= 0) this.y = this.radius + random(0, 2);
+    this.x += random(0, 0);
+    this.y += random(1, 4);
+    if (this.y > height) this.y = this.rainHeight * -1;
+
   },
 
   display: function () {
-    noStroke();
-    fill(102, 217, 255, 100);
-    ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+    strokeWeight(1);
+    stroke(5, 199, 25);
+    //fill(5, 199, 25);
+    //rect(this.x, this.y, this.rainWidth, this.rainHeight);
+    line(this.x, this.y, this.x + this.rainWidth, this.y + this.rainHeight);
   }
 
 };
--- sketch.js
+++ sketch.js
@@ -1,11 +1,37 @@
-var bubbles = [];
+var rain = [];
+
+var updateAndDisplay = function(rain) {
+  rain.update();
+  rain.display();
+};
+
+var addNumberRain = function (totalRain) {
+  for (var i = 0; i < totalRain; ++i) {
+    rain[i] = new Rain(random(0, width), random(0, height));
+  }
+};
 
 setup = function () {
-  createCanvas(600, 600);
-  // your code goes here
+  createCanvas(800, 800);
+  addNumberRain(200);
+
 };
 
 draw = function () {
   background(0);
-  // your code goes here
+  rain.forEach(updateAndDisplay);
+
 };
+
+
+/* -- using while loop
+
+  while (bubbles.length < totalBubbles) P
+    bubbles.push(new Bubble(random(0, width), random (0,height)));
+
+
+/* -- using forEach
+
+  bubbles.forEach(updateAndDisplay);
+  if (mouseIsPressed) bubbles.push(new Bubble(mouseX, mouseY));
+*/
